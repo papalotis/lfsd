ac1846a6c55afcbe07d699ce04b48e5f52073396
Panagiotis Karagiannis <papalotis1@gmail.com>
2023-09-23 19:46:14 +0200

    bumped version

--- lfsd/__init__.py
+++ lfsd/__init__.py
@@ -16,7 +16,7 @@ if platform.system() != "Windows":
         pass
         uvloop.install()
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 
 __all__ = ["LFSInterface", "ConeTypes", "ObservedCone", "LFSData"]
