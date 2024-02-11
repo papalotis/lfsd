c0cf3adc4cad04e8219cba4d41713974c87010d9
Panagiotis Karagiannis <papalotis1@gmail.com>
2024-02-11 15:21:39 +0100

    m

--- lfsd/__init__.py
+++ lfsd/__init__.py
@@ -15,7 +15,7 @@ if platform.system() != "Windows":
     else:
         uvloop.install()
 
-__version__ = "0.1.4.21"
+__version__ = "0.1.4.22"
 
 
 __all__ = ["LFSInterface", "ConeTypes", "ObservedCone", "LFSData"]
